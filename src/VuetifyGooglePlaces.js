4fcbea84684862bc56fe8680e2e7b7b76ce00ce1
Mark Waite <mark.a.waite@gmail.com>
2020-03-06 12:41:59 +1000

    [build] 0.1.10

--- src/VuetifyGooglePlaces.js
+++ src/VuetifyGooglePlaces.js
@@ -4,13 +4,17 @@ import { VTextField } from 'vuetify/lib'
 export default {
   mixins: [mixin],
   props: {
-    searchIcon: {
-      type: String,
-      default: () => 'search'
-    },
     clearIcon: {
       type: String,
       default: () => 'close'
+    },
+    prependIcon: {
+      type: String,
+      default: () => ''
+    },
+    searchIcon: {
+      type: String,
+      default: () => 'search'
     }
   },
   computed: {
@@ -29,6 +33,7 @@ export default {
         },
         props: {
           appendIcon: this.getAppendIcon,
+          prependIcon: this.prependIcon,
           value: this.val
         }
       })
